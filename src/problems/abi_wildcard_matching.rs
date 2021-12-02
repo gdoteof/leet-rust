82cb413c41f32d7a429c4c428a691c0e950f8225
Geoff Golder <geoffgolder@Geoffs-Mac-mini.local>
2021-12-02 16:00:03 -0500

    add cache else it's too slow

--- src/problems/abi_wildcard_matching.rs
+++ src/problems/abi_wildcard_matching.rs
@@ -6,22 +6,39 @@ pub fn is_match(s: String, p: String) -> bool {
         Single(u8),
         Empty
     }
+
+    // a patternsize+1 x candidatesize+1 cache
+    let dp:&mut Vec<Vec<Option<bool>>> = &mut vec!(vec!(None; s.len() + 1); p.len() + 1);
     
-    fn is_match_bytes(s: &[u8], p: &[u8]) -> bool {
-        println!("S:{:?}, P:{:?}", std::str::from_utf8(s),  std::str::from_utf8(p));
-        match parse(p){
+    fn is_match_bytes(s: &[u8], p: &[u8], dp: &mut Vec<Vec<Option<bool>>>) -> bool {
+        // println!("S:{:?}, P:{:?}", std::str::from_utf8(s),  std::str::from_utf8(p));
+        match dp[p.len()][s.len()] {
+            Some(b) => return b,
+            _ => ()
+        };
+
+        let res = match parse(p){
             (Pattern::Empty, _) => s.is_empty(),
-            (Pattern::ManyOrNone, _) if s.is_empty() => is_match_bytes(s, &p[1..]),
-            (Pattern::ManyOrNone, _) => is_match_single(s, Pattern::SingleAny, p) || is_match_bytes(&s[1..], p) || is_match_bytes(s, &p[1..]),
-            (single, _)  => is_match_single(s, single, p),
-        }
+            (Pattern::ManyOrNone, _) if s.is_empty() => is_match_bytes(s, &p[1..], dp),
+            (Pattern::ManyOrNone, _) => is_match_single(s, Pattern::SingleAny, p, dp) || is_match_bytes(&s[1..], p, dp) || is_match_bytes(s, &p[1..], dp),
+            (single, _)  => is_match_single(s, single, p, dp),
+        };
+
+        dp[p.len()][s.len()] = Some(res);
+
+        res
     }
 
-    fn is_match_single(s: &[u8], token: Pattern, p: &[u8]) -> bool {
+    fn is_match_single(s: &[u8], token: Pattern, p: &[u8], dp: &mut Vec<Vec<Option<bool>>>) -> bool {
+        match dp[p.len()][s.len()] {
+            Some(b) => return b,
+            _ => ()
+        };
+
         match s.split_first() {
             Some((head_s, tail_s)) => match token {
-                Pattern::Single(c) if *head_s == c => is_match_bytes(tail_s, &p[1..]),
-                Pattern::SingleAny => is_match_bytes(tail_s, &p[1..]),
+                Pattern::Single(c) if *head_s == c => is_match_bytes(tail_s, &p[1..], dp),
+                Pattern::SingleAny => is_match_bytes(tail_s, &p[1..], dp),
                 _ => false
             },
             None => p.is_empty()
@@ -39,7 +56,7 @@ pub fn is_match(s: String, p: String) -> bool {
 
     println!("entering");
     println!("S:{:?}, P:{:?}", s, p);
-    return is_match_bytes(&s.as_bytes(), &p.as_bytes())
+    return is_match_bytes(&s.as_bytes(), &p.as_bytes(), dp)
 }
 
 #[cfg(test)]
